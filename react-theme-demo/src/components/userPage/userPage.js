f3de2018fbea9dd544be10c3cfc72ad5bc2575ad
hoaiphuong <lequocphuong1022@gmai.com>
2022-12-20 23:22:11 +0700

    file

---
+++ react-theme-demo/src/components/userPage/userPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+const UserPage = props =>{
+    return(
+        <div>
+           day la page user
+        </div>
+    );
+}
+export default UserPage;
